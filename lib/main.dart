acf87434d2c396f0725a905a1dd781f7213b8298
Adil Ameen <adilameen3809@gmail.com>
2024-11-23 14:32:12 +0530

    firebase initialized for authendication

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:recipe_management_app/core/router/router.dart';
+import 'package:recipe_management_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await Hive.initFlutter();
   await Hive.openBox('shopping');
   await Hive.openBox('saved');
